f71688eafa90536f1a496d56b0d567eb2f9b5f32
YESSER TECH <aidenyassin115@gmail.com>
2024-10-14 09:56:57 +0300

    Update bmwtemp.js

--- src/plugin/bmwtemp.js
+++ src/plugin/bmwtemp.js
@@ -68,7 +68,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
                                 text: `Generated Temporary Email: ${tempEmail}`
                             }),
                             footer: proto.Message.InteractiveMessage.Footer.create({
-                                text: "© Powered By BMW MD"
+                                text: "© Powered By yessertech"
                             }),
                             header: proto.Message.InteractiveMessage.Header.create({
                                 title: "Temporary Email",
@@ -156,7 +156,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
                                 text: inboxMessages
                             }),
                             footer: proto.Message.InteractiveMessage.Footer.create({
-                                text: "© Powered By BMW MD"
+                                text: "© Powered By yessertech"
                             }),
                             header: proto.Message.InteractiveMessage.Header.create({
                                 title: "",
